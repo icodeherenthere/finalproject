da2b35c3ede59a63e487e0a2b5383f87c2021784
Malik Campbell-Greene <malikcampbellgreene@gmail.com>
2023-05-09 23:12:16 -0400

    working on calculator for calories and water

--- public/main.js
+++ public/main.js
@@ -1,15 +1,20 @@
 var add = document.getElementsByClassName("add");
 var calculate = document.getElementsByClassName('calculate');
 var gone = document.getElementsByClassName('deleteVehicles');
+var water = document.getElementsByClassName('ounces liters');
+var activity = document.getElementById("activity");
+var value = activity.value;
+var selectedOption = activity.options[activity.selectedIndex]
+var timeMultiplier = parseFloat(selectedOption.value);
 
-// function to calculate calories needed based on height and weight
-function calculateCalories(height, weight, sex, age, timeMultiplier) {
+// function to calculate calories needed based on height, weight, sex, age, and timeMultiplier
+function calculateCalories(height, weight, sex, age) {
   let x = 400 * timeMultiplier
   if (sex === 'male') {
-    let caloriesNeededM = 9.99 * (weight * 0.45359237) + 6.25 * (height * 2.54) - (4.92 * age + 5);
+    let caloriesNeededM = (9.99 * (weight * 0.45359237) + 6.25 * (height * 2.54) - (4.92 * age + 5)) * x;
     return caloriesNeededM;
   } else if (sex === 'female') {
-    let caloriesNeededF = 9.99 * (weight * 0.45359237) + 6.25 * (height * 2.54) - (4.92 * age - 161);
+    let caloriesNeededF = (9.99 * (weight * 0.45359237) + 6.25 * (height * 2.54) - (4.92 * age - 161)) * x;
     return caloriesNeededF;
   }
   console.log(height)
@@ -19,13 +24,46 @@ function calculateCalories(height, weight, sex, age, timeMultiplier) {
   console.log(sex)
 }
 
+// 8 ounces
+// 1 oz = 0.02957 l
+
+// function to calculate calories needed based on height and weight
+function calculateWater(height, weight, sex, age, timeMultiplier) {
+  
+  if (sex === 'male') {
+    let caloriesNeededM = 9.99 * (weight * 0.45359237) + 6.25 * (height * 2.54) - (4.92 * age + 5);
+    return caloriesNeededM;
+  } else if (sex === 'female') {
+    let caloriesNeededF = 9.99 * (weight * 0.45359237) + 6.25 * (height * 2.54) - (4.92 * age - 161);
+    return caloriesNeededF;
+  }
+  
+
+  console.log(height)
+  console.log(weight)
+  console.log(age)
+  console.log(timeMultiplier)
+  console.log(sex)
+}
+// finds out how muc calories someone needs
+Array.from(calculate).forEach(function(element){
+  element.addEventListener('click', function(e){
+    let calculatation = calculateWater(
+      document.getElementById('height'),
+      document.getElementById('weight'),
+      document.getElementById('sex'),
+      document.getElementById('age'),
+      parseFloat(document.getElementById('activity'))
+    )
+    document.getElementById('calCount').textContent = 'Calories Needed ' + calculatation
+})});
+
 Array.from(calculate).forEach(function(element){
   element.addEventListener('click', function(e){
     let calculatation = calculateCalories(
       document.getElementById('height'),
       document.getElementById('weight'),
       document.getElementById('sex'),
-      document.getElementById('age'),
       parseFloat(document.getElementById('activity'))
     )
     document.getElementById('calCount').textContent = 'Calories Needed ' + calculatation
