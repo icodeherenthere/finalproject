c46d7ba9ba557a6c1ac10ceaecaca6f8bb3a7988
Malik Campbell-Greene <malikcampbellgreene@gmail.com>
2023-06-12 10:58:30 -0400

    fixed fetch

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,6 @@
 const { model } = require("mongoose");
 const { tiresToUse } = require("../public/constants");
+const fetch = require("node-fetch");
 
 module.exports = function(app, passport, db, ObjectId) {
 
